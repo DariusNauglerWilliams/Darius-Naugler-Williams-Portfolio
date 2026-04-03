ea77f575386116541546bb50dc32189ed0dce7b0
W0468712 <W0468712@nscc.ca>
2026-04-03 20:52:47 -0300

    all html side set up

--- dnw-portfolio/src/About.jsx
+++ dnw-portfolio/src/About.jsx
@@ -12,18 +12,31 @@ function About() {
           alt="Profile"
           className="profile-pic"
         />
-        <div className="about-me-text">
+        <div className="about-me-info">
           <h1>About Me</h1>
           <p>
             Hi! I'm Darius, a passionate student with a love for creating
             innovative solutions. I have experience in full-stack development and enjoy
             working on projects that make a difference.
           </p>
-          <p>
-            When I'm not coding, I enjoy Excersizing, Music, and exploring
-            On the internet for countless things like new frameworks and languages and personal projects to do.
-          </p>
+          
         </div>
+        <div className="about-me-skills">
+          <h1>My Current Skills</h1>
+          <p>
+            React, JS, web development, problem-solving, teamwork, communication, adaptability, and a strong desire to learn and grow in the IT field.
+          </p>
+          </div>
+          <div className="about-me-looking-for">
+          <h1>What I'm Currently Looking For</h1>
+          <p>
+            I am currently looking for internship or job opportunities in the IT field where I can grow and gain experience.
+          </p>
+</div>
+
+
+
+
       </div>
 
 
--- dnw-portfolio/src/Resume.jsx
+++ dnw-portfolio/src/Resume.jsx
@@ -4,11 +4,21 @@ function Resume() {
 return(
     <section id="resume" style={{ height: "100vh" }}>
 
+    <div className="resume-shell">
+
+    <div className="resume-header">
+            <h2 className="resume-kicker">My Resume</h2>
+            
+          </div>
+
+<a href="/resume.pdf" target="_blank" rel="noopener noreferrer"></a>
+
+  <img className="resume-display" src="/resume.png" alt="resume"/>
 
 
-<p>this is my resume section</p>
+<a className="resume-download" href="/resume.pdf" download>Download My Resume</a>
 
-
+</div>
 
     </section>
 )
