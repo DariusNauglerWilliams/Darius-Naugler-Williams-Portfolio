189bb2c3c222981c92f7a9a3e25925acdf41eec1
W0468712 <W0468712@nscc.ca>
2026-04-03 17:00:16 -0300

    font change

--- dnw-portfolio/src/cta.jsx
+++ dnw-portfolio/src/cta.jsx
@@ -11,7 +11,7 @@ function Cta() {
        <div style={{ position: "absolute", top: "37%", left: "10%" }}>
         <h1 style={{ fontSize: "90px", margin: 0 }}>Let’s Work</h1>
         <a href="#resume">
-          <button style={{ fontSize: "20px" }}>Hire Me</button>
+          <button style={{ fontSize: "24px" }}>Hire Me</button>
         </a>
       </div>
     </section>
