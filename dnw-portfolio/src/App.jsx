370cc2d5f8f5fae20e24e4ee1369f2ad35a9c713
W0468712 <W0468712@nscc.ca>
2026-04-03 14:32:53 -0300

    working

--- dnw-portfolio/src/App.jsx
+++ dnw-portfolio/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-
+import CustomNavbar from './CustomNavbar';
+import About from './About';
+import Projects from './Projects';
+import Resume from './Resume';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
   return (
     <div>
 
+    <CustomNavbar />
+     <About />
+      <Projects />
+      <Resume />
       
   </div>
   );
--- dnw-portfolio/src/main.jsx
+++ dnw-portfolio/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import CustomNavbar from './CustomNavbar';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CustomNavbar />
+   
     <App />
+   
   </StrictMode>
 );
