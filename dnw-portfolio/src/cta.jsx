e47a9dbd7c65c8843e13cb78411ab4b1d78b243c
W0468712 <W0468712@nscc.ca>
2026-04-05 21:04:56 -0300

    fit maybe

--- dnw-portfolio/src/cta.jsx
+++ dnw-portfolio/src/cta.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 function Cta() {
   return (
     <section id="cta" style={{position: "relative"}}> 
-      <img id="img" src="/dariuscta.png" alt="cta" style={{ width: "100%" }} />
+      <img id="img" src="/dariuscta.png" alt="cta" style={{ width: "100%", height: "100vh", objectFit: "cover" }} />
 
 
 
 
-       <div style={{ position: "absolute", top: "30%", left: "6%" }}>
-        <h1 style={{ fontSize: "90px", margin: 0 }}>Let’s Work</h1>
+       <div style={{ position: "absolute", top: "37%", left: "10%" }}>
+        <h1 style={{ fontSize: "90px", margin: 0, color: "#000000" }}>Let’s Work</h1>
         <a href="#resume">
-          <button style={{ fontSize: "24px" }}>Hire Me</button>
+          <button style={{ fontSize: "24px" , color: "#000000"}}>Hire Me</button>
         </a>
       </div>
     </section>
