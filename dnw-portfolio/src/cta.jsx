1178982da60893e1362baf158fc83fc46a5e7a79
W0468712 <W0468712@nscc.ca>
2026-04-05 21:08:24 -0300

    try again 2nd browser still on  me

--- dnw-portfolio/src/cta.jsx
+++ dnw-portfolio/src/cta.jsx
@@ -8,7 +8,7 @@ function Cta() {
 
 
 
-       <div style={{ position: "absolute", top: "37%", left: "2%" }}>
+       <div style={{ position: "absolute", top: "37%", left: "1.2%" }}>
         <h1 style={{ fontSize: "90px", margin: 0, color: "#000000" }}>Let’s Work</h1>
         <a href="#resume">
           <button style={{ fontSize: "24px" , color: "#000000"}}>Hire Me</button>
